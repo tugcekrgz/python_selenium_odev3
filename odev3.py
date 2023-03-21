3e8dcb73c0a0804b76ea8afd019259f81eff71a3
tugcekrgz <tugcekrgz28@gmail.com>
2023-03-22 00:18:38 +0300

    python selenium ödev3
    kodlama.io python selenium kursundaki üçüncü ödevim

---
+++ odev3.py
@@ -0,0 +1,63 @@
+# HTML NEDİR?
+
+# HTML(Hyper Text Markup Language)=  Web sayfalarını oluşturmak için kullanılan standart metin işaretleme dilidir.
+#                                    HTML,bir programlama dili olarak tanımlanamaz, kodlarıyla kendi başına çalışan 
+#                                    bir program yazılamaz.Ancak bu dili yorumlayabilen programlar aracılığıyla 
+#                                    çalışabilen programlar yazılabilir.web tasarımcılarına sayfalar ve uygulamalar
+#                                    için profilleri ,bağlantılar, blok alıntılar,paragraflar ve başlıklar oluştur-
+#                                    malarında yardımcıdır.
+
+
+
+#   EN ÇOK KULLANILAN HTML KODLARI ;
+
+# <!DOCTYPE html> : Tüm HTML belgeleri bu kod ile başlamak zorundadır.Bu kod sayfanın HTML5 formatına göre kodlandığını gösterir.
+ 
+# <html> : Kök etiket olarakta bilinen bu temel kullanımda tüm HTML kodları,bu iki etiket arasıda tanımlanmak zorundadır.
+
+# <body> : Görünebilir tüm HTML kod parçalarının eklendiği etiket alanıdır.
+
+# <head> : Web sayfasının dili, başlığı ve diğer düzenlemelerinin yer aldığı etikettir.
+
+# <title> : Web sitesinin başlık alanını tanımlamak için kullanılır.<title> etiketi,<head> etiketleri arasında olmak zorundadır.
+
+
+
+
+# HTML LOCATORS ?
+
+#  HTML LOCATORS(Konumlandırcı)=Web sayfalarında öğeler, genellikle HTML etiketleri ve özellikleri kullanılarak tanımlanır.
+#                               HTML locators,öğeleri tanımlama yöntemlari olarak kullanılır ve web sayfalrı test otomasyonu (selenium vb)
+#                               işlemlerinde sıklıkla kullanılır.
+
+#  KULLANILAN YÖNTEMLER;
+# 
+# 1-ID 
+# 2-ClassName
+# 3-TagName 
+# 4-Name 
+# 5-XPATH (öğenin konumu belirlemek için kullanılır.)  
+# 6-LinkText
+# 7-CssSelector
+
+
+# SELENİUM AKSİYONLARI
+
+# click()=Seçilen öğeye tıklamak için kullanılır.
+
+# clear()=Seçilen öğenin içeriğini temizlemek için kullanılır.
+
+# submit()=Bir formu göndermek için kullanılır.
+
+# get()=bir URL yi tarayıcıda açmak için kullanılır.
+
+# back()=Tarayıcıda bir önceki sayfaya geri dönmek için kullanılır.
+
+# forward()=Trayıcıda bir sonraki sayfaya gitmek için kullanılır .
+
+# refresh()=Sayfayı yenilemek için kullanılır.
+
+# select_by_visible_text= Bir dropdown öğesinde belirli bir metne tıklamak için kullanılır.
+
+# scroll()=Sayfayı aşağı veya yukarı kaydırmak için kullanılır.
+
